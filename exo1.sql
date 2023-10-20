227a2b68fd64e5fc3ee48206553fbc038c4083f4
Jérôme Chaussin <jerome.chaussin@aol.com>
2023-10-20 15:07:25 +0200

    Création table 'Produits'

--- exo1.sql
+++ exo1.sql
@@ -5,3 +5,18 @@ CREATE DATABASE IF NOT EXISTS exo1 CHARACTER SET="utf8" COLLATE="utf8_general_ci
 
 --On rentre dans la database
 USE exo1;
+
+--Création table 'Produits'
+CREATE TABLE produits (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nom VARCHAR(100),
+    Description TEXT,
+    Prix DECIMAL(10, 2),
+    Stock INT,
+    Fabricant VARCHAR(50),
+    DateAjout DATE,
+    Categorie VARCHAR(50),
+    EnStock BOOLEAN,
+    Poids DECIMAL(7, 2),
+    DateExpiration DATE
+);
