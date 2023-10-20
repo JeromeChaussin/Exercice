796f4a23b4c807f8167a540091046ebf7e4100fa
Jérôme Chaussin <jerome.chaussin@aol.com>
2023-10-20 15:05:31 +0200

    Utilisation de exo1

--- exo1.sql
+++ exo1.sql
@@ -2,3 +2,6 @@
 
 --Création de la database exo1 si ellle n'hesiste pas, en utf8
 CREATE DATABASE IF NOT EXISTS exo1 CHARACTER SET="utf8" COLLATE="utf8_general_ci";
+
+--On rentre dans la database
+USE exo1;
